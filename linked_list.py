76a2e9c7157ce854d8cef944a89f5c496fb426d9
Josh AbyPro <jm6.abypro@gmail.com>
2021-09-01 10:51:47 -0500

    the remove method completed

--- linked_list.py
+++ linked_list.py
@@ -35,22 +35,49 @@ class LinkedList(object):
     self.tail = self.init
 
   def checkNode(self, node, data):
+    if node == None:
+      return False
     if data == node.getData():
       return node.getNext()
     else:
       self.aux = node
     return self.checkNode(node.getNext(), data)
 
+  def deleteNode(self, init, data):
+    node = self.checkNode(init, data)
+    if self.aux != None:
+      self.aux.setNext(node)
+    else:
+      self.init = node
+    if not node:
+      return False
+    return True
+
+  def indexNode(self, node, index):
+    if index == 0:
+      return node.getNext()
+    else:
+      self.aux = node
+    index -= 1
+    return self.indexNode(node.getNext(), index)
+
   def add(self, data):
     new_node = Node(data)
     self.tail.setNext(new_node)
     self.tail = new_node
 
-  # mode -> 'first' (default), 'last', 'all', 'index'
+  # mode -> 'first' (default), 'all', 'index'
   def remove(self, data, mode = 'first'):
+    self.aux = None
     if mode == 'first':
-      self.aux = None
-      node = self.checkNode(self.init, data)
+      self.deleteNode(self.init, data)
+    elif mode == 'all':
+      while True:
+        res = self.deleteNode(self.init, data)
+        if not res:
+          return
+    elif mode == 'index':
+      node = self.indexNode(self.init, data)
       if self.aux != None:
         self.aux.setNext(node)
       else:
