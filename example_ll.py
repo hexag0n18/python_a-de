285220d58458e66ad5e0aebbee772d9b4cb7b577
Josh AbyPro <jm6.abypro@gmail.com>
2021-09-01 07:53:52 -0500

    create example linked list file

---
+++ example_ll.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+from linked_list import LinkedList
+
+linked_list = LinkedList(1)
+linked_list.add(2)
+linked_list.add(3)
+linked_list.add(4)
+linked_list.add(5)
+print(linked_list.printer())
