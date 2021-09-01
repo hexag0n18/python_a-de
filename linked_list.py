cbfb98f26ac8fa8a9e4dd71dcbc38964ae87fadd
Josh AbyPro <jm6.abypro@gmail.com>
2021-09-01 07:50:52 -0500

    cleanning - 1

--- linked_list.py
+++ linked_list.py
@@ -1,11 +1,21 @@
-#!/bin/python3
+#! /usr/bin/env python
 
+# LISTA ENLAZADA
+
+# CLASE NODO
+'''
+  data    -> guarda el dato del nodo en la lista enlazada
+  _next_  -> guarda la direccion al siguiente nodo de la lista
+'''
 class Node(object):
   data = 0
-  _next_ = 'null'
-  def __init__(self, data, _next_ = 'null'):
+  _next_ = None
+  def __init__(self, data, _next_ = None):
     self.data = data
     self._next_ = _next_
+
+  def setData(self, data):
+    self.data = data
 
   def setNext(self, node):
     self._next_ = node
@@ -17,31 +27,22 @@ class Node(object):
     return self._next_
 
 class LinkedList(object):
-  init = 'null'
-  tail = 'null'
+  init = None
+  tail = None
   def __init__(self, data):
     self.init = Node(data)
     self.tail = self.init
 
-  def createNode(self, data):
+  def add(self, data):
     new_node = Node(data)
     self.tail.setNext(new_node)
     self.tail = new_node
 
   def getDataNode(self, node):
     data = f"{node.getData()}"
-    if node.getNext() != 'null':
+    if node.getNext():
       data += ' -> ' + self.getDataNode(node.getNext())
     return data
 
   def printer(self):
     print(self.getDataNode(self.init))
-    # print(self.init.getData())
-
-
-linked_list = LinkedList(1)
-linked_list.createNode(2)
-linked_list.createNode(3)
-linked_list.createNode(4)
-linked_list.createNode(5)
-print(linked_list.printer())
